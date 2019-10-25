121e9eda4cd8e7a9dc1609b75766ccb146e78bd2
sepehr71 <sepehrshakeri7@gmail.com>
2019-10-25 22:25:12 +0330

    ee

--- script.js
+++ script.js
@@ -1,99 +1,83 @@
 let piclength= 33;
 let page = 1;
-let pagesearch = 1;
-let equlizer=0;
 
 function clearpage()
 {
-    var elem = document.querySelector('#masonary').innerHTML="";
+    var elem = document.querySelector('#masonry').innerHTML="";
 }
-
-let  requestURL = 'https://picsum.photos/v2/list?page='+page+'&limit='+piclength;
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function()
+function prepare()
 {
-    for(var i=0;i<piclength;i++)
+    
+}
+function show()
+{
+    let  requestURL = 'https://picsum.photos/v2/list?page='+page+'&limit='+piclength;
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function()
     {
-       
+     for(var i=0;i<piclength;i++)
+     {
        let requestresponse1 = request.response[i]['download_url'];
        let requestresponse2 = request.response[i]['url'];
+
        let card = document.createElement('div');
        card.setAttribute("id" , "container");
+
        let image = document.createElement('img');
        image.setAttribute("id","images");
+
        let shareicon = document.createElement('button');
-       shareicon.setAttribute("id", "sharepicbutton");
-       
+       shareicon.setAttribute("id", "sharebtn");
+       shareicon.innerHTML="⤴";
+
+       let likeicon = document.createElement('button');
+       likeicon.setAttribute("id", "likebtn");
+       likeicon.innerHTML="❤";
+
+       let dwlicon = document.createElement('button');
+       dwlicon.setAttribute("id", "dwlbtn");
+       dwlicon.innerHTML="⬇";
+
        image.src=requestresponse1;
        shareicon.src =  requestresponse2;
-       document.querySelector('#masonary').append(card);
-       document.querySelector('#masonary').appendChild(image);
-       document.querySelector('#masonary').appendChild(shareicon);
-    // image.appendChild(shareicon);
+       document.querySelector('#masonry').append(card);
+       document.querySelector('#masonry').appendChild(image);
+       document.querySelector('#masonry').appendChild(shareicon);
+       document.querySelector('#masonry').appendChild(likeicon);
+       document.querySelector('#masonry').appendChild(dwlicon);
+
        card.appendChild(image);
        card.appendChild(shareicon);
+       card.appendChild(likeicon);
+       card.appendChild(dwlicon);
+       document.getElementById('lbltipAddedComment').innerHTML = page;
     }
+  }
 }
 
 function nextpage()
 {
-    if(page==10)
-    {
-        page=0;
-    }
+    if(page==10){page=0;}
     clearpage();
     page++;
-    let  requestURL = 'https://picsum.photos/v2/list?page='+page+'&limit='+piclength;
-    let request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function()
-    {
-        for(var i=equlizer;i<piclength;i++)
-        {
-           let requestresponse = request.response[i]['download_url'];
-           let choice = document.createElement('img');
-           choice.src=requestresponse;
-           document.querySelector('#masonary').appendChild(choice);
-        }
-    }
-    document.getElementById('lbltipAddedComment').innerHTML = page;
+    show();
 }
 
 function prevpage()
 {
-    if(page==1)
-    {
-        page=11;
-    }
+    if(page==1){page=11;}
     clearpage();
     page--;
-    let  requestURL = 'https://picsum.photos/v2/list?page='+page+'&limit='+piclength;
-    let request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function()
-    {
-        for(var i=0;i<piclength;i++)
-        {
-           let requestresponse = request.response[i]['download_url'];
-           let choice = document.createElement('img');
-           choice.src=requestresponse;
-           document.querySelector('#masonary').appendChild(choice);
-        }
-    }
-    document.getElementById('lbltipAddedComment').innerHTML = page;
+    show();
 }
      
 function search() 
 {
     clearpage();
-    let search = document.getElementById('#textbox').value;
+    let typesearch = document.getElementById('#textbox').value;
     for(let x=1; x<10 ; x++)
     {
         let  requestURL = 'https://picsum.photos/v2/list?page='+x+'&limit=100';
@@ -105,19 +89,45 @@ function search()
         {
             for(let i=0;i<100;i++)
             {
-                let search1 = request.response[i]['author'];
-                if(search === search1)
+                let serversearch = request.response[i]['author'];
+                if(typesearch === serversearch)
                 {
-                    let requestresponse = request.response[i]['download_url'];
-                    let choice = document.createElement('img');
-                    choice.src=requestresponse;
-                    document.querySelector('#masonary').appendChild(choice);
+                    let requestresponse2 = request.response[i]['url'];
+                    let requestresponse1 = request.response[i]['download_url'];
+                    let card = document.createElement('div');
+                    card.setAttribute("id" , "container");
+
+                    let image = document.createElement('img');
+                    image.setAttribute("id","images");
+
+                    let shareicon = document.createElement('button');
+                    shareicon.setAttribute("id", "sharebtn");
+                    shareicon.innerHTML="⤴";
+
+                    let likeicon = document.createElement('button');
+                    likeicon.setAttribute("id", "likebtn");
+                    likeicon.innerHTML="❤";
+
+                    let dwlicon = document.createElement('button');
+                    dwlicon.setAttribute("id", "dwlbtn");
+                    dwlicon.innerHTML="⬇";
+
+                    image.src=requestresponse1;
+                    shareicon.src =  requestresponse2;
+                    document.querySelector('#masonry').append(card);
+                    document.querySelector('#masonry').appendChild(image);
+                    document.querySelector('#masonry').appendChild(shareicon);
+                    document.querySelector('#masonry').appendChild(likeicon);
+                    document.querySelector('#masonry').appendChild(dwlicon);
+
+                    card.appendChild(image);
+                    card.appendChild(shareicon);
+                    card.appendChild(likeicon);
+                    card.appendChild(dwlicon);
+                    document.getElementById('lbltipAddedComment').innerHTML = page;
+                
                 }
             }
         }
     }   
-}
-function returntopage()
-{
-
 }
